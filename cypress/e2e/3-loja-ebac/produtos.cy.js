455d5fcb8a4775a7f3fea8c62b0eabcb309dd532
Augusto Teixeira <teixeira.augusto1@gmail.com>
2025-10-07 15:51:50 -0300

    config e fixture

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -3,16 +3,13 @@
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')         //Na pagina 'produtos' do site 'lojaebac.com.br'
+        cy.visit('produtos')         
     });
 
     it('Deve selecionar um produto da lista', () => {       
-        cy.get('.name > a')                                             //dentro da lista que contém os nomes dos elementos da página, escolha:
-        //.first()                                                      //primeiro item da lista      
-        //.last()                                                       //ultimo item da lista
-        //.eq(2)                                                        //terceiro item da lista (0, 1, 2, ...)
-        .contains('Ajax Full-Zip Sweatshirt')                         //Nome do elemento na lista
-        .click()                                                        //e dê um clique neste elemento
+        cy.get('.name > a')                                             
+        .contains('Ajax Full-Zip Sweatshirt')                         
+        .click()                                                        
         
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
